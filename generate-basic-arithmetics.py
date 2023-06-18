2dd3d5c8cc1b32e441c15a5558d7f92d16169b8f
Christoph Schuhmann <christoph_s@freenet.de>
2023-06-18 12:00:00 +0200

    Update generate-basic-arithmetics.py

--- generate-basic-arithmetics.py
+++ generate-basic-arithmetics.py
@@ -1,47 +1,65 @@
 from sympy.functions.elementary.integers import ceiling
-import basic_arithmetic_functions
+import basicmathsfunctions
 import inspect
+import itertools
+import random
+
 
 # Get all global functions from the mathfunctions module
 global_functions = [
     name
-    for name, obj in inspect.getmembers(mathfunctions)
-    if inspect.isfunction(obj) and inspect.getmodule(obj) == mathfunctions
+    for name, obj in inspect.getmembers(basicmathsfunctions)
+    if inspect.isfunction(obj) and inspect.getmodule(obj) == basicmathsfunctions
 ]
-
-
-num_samples_total=10
+ 
+num_samples_total = 100
 
 def execute_functions(function_dict, n):
-    import basic_arithmetic_functions
+    import basicmathsfunctions
+    
+    results = []
     
     for function_name in function_dict:
-        function = getattr(basic_arithmetic_functions, function_name, None)
+        function = getattr(basicmathsfunctions, function_name, None)
         
         if function and callable(function):
             try:
                 result = function(function_dict[function_name])
-                print(function_name, ": ", result)
+                results.append(result)
             except Exception as e:
                 print("Error:", function_name, "-", str(e))
         else:
             print("Invalid function:", function_name)
+    
+    return results
 
-            
 def add_values_in_dictionary(dictionary):
     total = 0
     for value in dictionary.values():
         total += value
     return total
 
-# Beispielaufruf
+# Create a dictionary of function names and assign the desired number of samples to each function
 functions = {}
-
-# Print the list of function names
+num_samples_per_function = ceiling(num_samples_total / len(global_functions))
 for function_name in global_functions:
-    functions [function_name]= ceiling(num_samples_total /len(global_functions))
-print(global_functions)
-result = add_values_in_dictionary(functions)
-print("Sum of all ordered math problems in the dict:", result)
+    #print(num_samples_per_function)
+    functions[function_name] = num_samples_per_function
 
-execute_functions(functions, num_samples_total)
+# Get the list of results
+result_list = execute_functions(functions, num_samples_total)
+
+concatenated_list = list(itertools.chain(*result_list))
+
+# Shuffle the concatenated list
+random.shuffle(concatenated_list)
+
+# Print the shuffled list
+print("Shuffled list:")
+for item in concatenated_list:
+    print(item)
+
+
+# Calculate and print the sum of results
+result_sum = add_values_in_dictionary(functions)
+print("Sum of all ordered math problems in the dict:", result_sum)
